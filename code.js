d81229e6579fb50913ebeb3414f5816ef2325624
Tyler Smith <oats000@hotmail.com>
2016-05-18 19:20:41 -0400

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
 'use strict';
+function favIceCream(icecream) {
+  return "I love " + icecream;
+}
+
+function shouting(string) {
+  return string.toUpperCase();
+}
+
+function roundDown(num){
+  return Math.floor(num);
+}
+
+
+function theTruth(){
+  return true;
+}
